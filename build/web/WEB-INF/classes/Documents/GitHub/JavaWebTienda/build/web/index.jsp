67f06adfe7f422093c1591a09aebd91115a38a50
taylorbernat <taylorfabianbernate@gmail.com>
2025-05-30 09:11:09 -0500

    mierda

--- build/web/WEB-INF/classes/Documents/GitHub/JavaWebTienda/build/web/index.jsp
+++ build/web/WEB-INF/classes/Documents/GitHub/JavaWebTienda/build/web/index.jsp
@@ -1,17 +1,32 @@
-<%-- 
-    Document   : index
-    Created on : 20 may 2025, 8:13:29 p.m.
-    Author     : TOLOZA
---%>
+
 
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
     <head>
+        <link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/Style/StyleControlCorralHuerto  .css">  
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>JSP Page</title>
     </head>
     <body>
-        <h1>Hello World!sss </h1>
+        <h1> Control de corrales </h1>
+         <form action="SAlto" method="GET">
+            <label>digite el numero de corral:</label>
+            <input type "text" name="idCorral"></br>
+            
+            <label>digite la cantidad de animales:</label>
+            <input type="number" name="cantidadAnimales"></br>
+            
+            <label>digite el estado de los corrales:</label>
+            <select id="estado">
+                    <option value="Buen estado">Buen estado</option>
+                    <option value="Mal estado">Mal estado</option>
+                    
+                </select><br>
+                
+            <label>digite el registro de limpieza :</label>
+            <input type="date" name="corralesLimpios"></br>
+            <input type="submit"name="enviarinfo">
+        </form>
     </body>
 </html>
--- src/java/Controlador/SAlto.java
+++ src/java/Controlador/SAlto.java
@@ -1,9 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
+
 package Controlador;
 
+import Modelo.Persona;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -11,23 +9,12 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 
-/**
- *
- * @author SENA
- */
+
 @WebServlet(name = "SAlto", urlPatterns = {"/SAlto"})
 public class SAlto extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -44,44 +31,37 @@ public class SAlto extends HttpServlet {
             out.println("</html>");
         }
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
+      @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
+        
+        String idCorral = request.getParameter("idCorral");
+        String cantidadAnimales = request.getParameter("cantidadAnimales");
+        String estadoCorrales = request.getParameter("estadoCorrales");
+        int corralesLimpios =Integer.parseInt(request.getParameter("corralesLimpios"));
+        
+        Persona  objPersona  = new Persona (idCorral,cantidadAnimales,estadoCorrales,corralesLimpios);
+        objPersona .imprimir_dato();
+        
+        ArrayList<Persona >listaPersonas  = new ArrayList<>();
+        listaPersonas.add(new  Persona  ("4567", "pablo", "perez", 300));
+        listaPersonas.add(new Persona  ("0987", "ricardo", "garcia", 200));
+        listaPersonas.add(new Persona  ("1234","taylor", "bernate", 100));
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
 
 }
+
+
+
