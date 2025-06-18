ea222bb9ab6e7bd8df93ecf744ed74ceef1e305c
Esteban-bit-dot <eandrescardales@gmail.com>
2025-06-18 15:50:38 -0500

    Update Produccion_Agricola.java

--- src/java/Modelo/Produccion_Agricola.java
+++ src/java/Modelo/Produccion_Agricola.java
@@ -1,9 +1,8 @@
-
 package Modelo;
 import java.sql.*;
 
 
-public class Produccion_Agricola{
+   public class Produccion_Agricola {
     public String usuario;
     public String url;
     public String clave;
@@ -20,7 +19,15 @@ public class Produccion_Agricola{
     }
     
     public void hacerConexion() throws SQLException {
+        
+        
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
 
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        
         try {
             conex = DriverManager.getConnection(url, usuario, clave);
 
@@ -29,24 +36,23 @@ public class Produccion_Agricola{
         }
     }
     
-    public ResultSet getCedula() throws SQLException{
-        String sqlText="SELECT cedula FROM persona";
+    public ResultSet getid() throws SQLException{
+        String sqlText="SELECT id FROM iregistro_produccion";
         Statement consultarSentencia =this.conex.createStatement();
         this.resultadoConsulta = consultarSentencia.executeQuery(sqlText);
-        
         return resultadoConsulta;
     }
     
-    public ResultSet getCorreo() throws SQLException {
-        String sqlText = "SELECT correo	 FROM persona";
+    public ResultSet getanimal() throws SQLException {
+        String sqlText = "SELECT animal FROM iregistro_produccion";
         Statement consultarSentencia = this.conex.createStatement();
         this.resultadoConsulta = consultarSentencia.executeQuery(sqlText);
         return this.resultadoConsulta;
         
     }
     
-    public ResultSet getContraseña() throws SQLException {
-        String sqlText = "SELECT contraseña FROM persona";
+    public ResultSet getCantidad() throws SQLException {
+        String sqlText = "SELECT cantidad FROM iregistro_produccion";
         Statement consultaSentencia = this.conex.createStatement();
         this.resultadoConsulta =consultaSentencia.executeQuery(sqlText);
         return this.resultadoConsulta;
@@ -54,29 +60,36 @@ public class Produccion_Agricola{
     
     
     
-    public ResultSet getNombre() throws SQLException {
-        String sqlText = "SELECT nombre FROM persona";
+    public ResultSet getestado() throws SQLException {
+        String sqlText = "SELECT estado FROM iregistro_produccion";
+        Statement consultaSentencia = this.conex.createStatement();
+        this.resultadoConsulta = consultaSentencia.executeQuery(sqlText);
+        return this.resultadoConsulta;
+    }
+    
+        public ResultSet getfecha() throws SQLException {
+        String sqlText = "SELECT fecha FROM iregistro_produccion";
         Statement consultaSentencia = this.conex.createStatement();
         this.resultadoConsulta = consultaSentencia.executeQuery(sqlText);
         return this.resultadoConsulta;
     }
 
     
-    public boolean confirmarInformacionRegistro(int dato) throws SQLException{
-        String sqlText = "SELECT cedula	 FROM persona WHERE cedula = ?";
+    public boolean confirmarInformacionRegistro(String dato) throws SQLException{
+        String sqlText = "SELECT id FROM iregistro_produccion WHERE id = ?";
         PreparedStatement consultarSentencia = this.conex.prepareStatement(sqlText);
-        consultarSentencia.setInt(1, dato);
+        consultarSentencia.setString(1, dato);
         this.resultadoConsulta = consultarSentencia.executeQuery();
 
         System.out.println(consultarSentencia);
         
         
         if( this.resultadoConsulta.next()){
-            String cedulas =  this.resultadoConsulta.getString("cedula");
-            System.out.println(cedulas);
-            if (cedulas == null) {
+            String id =  this.resultadoConsulta.getString("id");
+            System.out.println(id);
+            if (id == null) {
                 return true;
-            } else if (cedulas.equals(dato)) {
+            } else if (id.equals(dato)) {
                 return false;
             } else {
                 return false;
@@ -87,18 +100,39 @@ public class Produccion_Agricola{
         }
     }
     
-    public boolean agregarUsuario(int cedula, String nombre,String correo,String contraseña) throws SQLException{
-        String textosql = "INSERT INTO persona(cedula, nombre, correo, contraseña) VALUES (?, ?, ?, ?)";
+    public boolean agregarUsuario(int id, String animales,int cantidad, String estado, String fecha) throws SQLException{
+        String textosql = "INSERT INTO iregistro_produccion(id, animal, cantidad, estado, fecha) VALUES (?, ?, ?, ?, ?)";
         PreparedStatement modific = this.conex.prepareStatement(textosql);
-        modific.setInt(1, cedula);
-        modific.setString(2, nombre);
-        modific.setString(3, correo);
-        modific.setString(4, contraseña	);
+        modific.setInt(1, id);
+        modific.setString(2, animales);
+        modific.setInt(3, cantidad);
+        modific.setString(4, estado);
+        modific.setString(5, fecha);
 
 
         int filasInsertadas = modific.executeUpdate();
 
         return  filasInsertadas>0;
     
+    }
+    public boolean agregarUsuario2(int id, String huerto,int cantidad, String estado, String fecha) throws SQLException{
+        String textosql = "INSERT INTO iregistro_produccion(id, huerto, cantidad, estado, fecha) VALUES (?, ?, ?, ?, ?)";
+        PreparedStatement modific = this.conex.prepareStatement(textosql);
+        modific.setInt(1, id);
+        modific.setString(2, huerto);
+        modific.setInt(3, cantidad);
+        modific.setString(4, estado);
+        modific.setString(5, fecha);
+
+
+        int filasInsertadas = modific.executeUpdate();
+
+        return  filasInsertadas>0;
+    
+    }
+
+ 
+    public boolean confirmarInformacionRegistro(int identificador) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
